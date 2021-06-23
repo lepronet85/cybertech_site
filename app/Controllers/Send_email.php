3fe74a94432a83efd14ee64f531c704cc74cdfbc
lepronet85 <lepronet85@gmail.com>
2021-06-23 19:13:51 +0100

    first commit

--- app/Controllers/Send_email.php
+++ app/Controllers/Send_email.php
@@ -7,29 +7,45 @@ class Send_email extends BaseController
 	public function index()
 	{
 
-        if ($this->request->getMethod() === 'post' && $this->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'subject' => 'required',
-            'message' => 'required'
-        ])) 
-        {
+        // if ($this->request->getMethod() === 'post' && $this->validate([
+        //     'name' => 'required',
+        //     'email' => 'required',
+        //     'subject' => 'required',
+        //     'message' => 'required'
+        // ])) 
+        // {
 
-            $email = \Config\Services::email();
+        //     $email = \Config\Services::email();
 
-            $email->setFrom($this->request->getPost('email'), $this->request->getPost('name'));
-            $email->setTo('abdoulazizsoumana85@gmail.com');
+        //     $email->setFrom($this->request->getPost('email'), $this->request->getPost('name'));
+        //     $email->setTo('abdoulazizsoumana85@gmail.com');
 
-            $email->setSubject($email->request->getPost('subject'));
-            $email->setMessage($email->request->getPost('message'));
+        //     $email->setSubject($email->request->getPost('subject'));
+        //     $email->setMessage($email->request->getPost('message'));
 
-            //
-            if ($email->send())
-            {
-                echo 'OK';
-            }
+        //     //
+        //     if ($email->send())
+        //     {
+        //         echo 'OK';
+        //     }
    
+        // }
+
+        $email = \Config\Services::email();
+
+        $email->setFrom('your@example.com', 'Your Name');
+        $email->setTo('abdoulazizsoumana85@gmail.com');
+        $email->setCC('another@another-example.com');
+        $email->setBCC('them@their-example.com');
+
+        $email->setSubject('Email Test');
+        $email->setMessage('Testing the email class.');
+
+        if ($email->send())
+        {
+            echo 'OK';
         }
+
 	}
 
 }
