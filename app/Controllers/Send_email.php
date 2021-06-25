214603006413fd3bee26cfe953ea5d66d2aaed89
lepronet85 <lepronet85@gmail.com>
2021-06-25 16:28:52 +0100

    first commit

--- app/Controllers/Send_email.php
+++ app/Controllers/Send_email.php
@@ -6,46 +6,36 @@ class Send_email extends BaseController
 {
 	public function index()
 	{
+        $name = $this->request->getPost('name');
+        $from = $this->request->getPost('email');
+        $subject = $this->request->getPost('subject');
+        $message = $this->request->getPost('message');
 
-        // if ($this->request->getMethod() === 'post' && $this->validate([
-        //     'name' => 'required',
-        //     'email' => 'required',
-        //     'subject' => 'required',
-        //     'message' => 'required'
-        // ])) 
-        // {
 
-        //     $email = \Config\Services::email();
-
-        //     $email->setFrom($this->request->getPost('email'), $this->request->getPost('name'));
-        //     $email->setTo('abdoulazizsoumana85@gmail.com');
-
-        //     $email->setSubject($email->request->getPost('subject'));
-        //     $email->setMessage($email->request->getPost('message'));
-
-        //     //
-        //     if ($email->send())
-        //     {
-        //         echo 'OK';
-        //     }
-   
-        // }
-
-        $email = \Config\Services::email();
-
-        $email->setFrom('your@example.com', 'Your Name');
-        $email->setTo('abdoulazizsoumana85@gmail.com');
-        $email->setCC('another@another-example.com');
-        $email->setBCC('them@their-example.com');
-
-        $email->setSubject('Email Test');
-        $email->setMessage('Testing the email class.');
-
-        if ($email->send())
+        if ($this->request->getMethod() === 'post' && $this->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'subject' => 'required',
+            'message' => 'required'
+        ])) 
         {
-            echo 'OK';
-        }
 
+            $email = \Config\Services::email();
+
+            $email->setFrom($from, $name);
+            $email->setTo('abdoulazizsoumana85@gmail.com');
+
+            $email->setSubject($subject);
+            $email->setMessage($message);
+
+            if ($email->send())
+            {
+                echo 'OK';
+            }
+   
+            echo "Nom : ".$name."E-mail : ".$from."Sujet : ".$subject."Message : ".$message;
+
+        }
 	}
 
 }
