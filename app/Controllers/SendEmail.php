6165ff0d223fb9a180f20a09c4ca9d11bcb64ef9
lepronet85 <lepronet85@gmail.com>
2021-06-25 17:56:49 +0100

    first commit

--- app/Controllers/SendEmail.php
+++ app/Controllers/SendEmail.php
@@ -2,37 +2,27 @@
 
 namespace App\Controllers;
 
-class SendEmail extends BaseController
+class Send_email extends BaseController
 {
 	public function index()
 	{
-        $name = $this->request->getPost('name');
-        $from = $this->request->getPost('email');
-        $subject = $this->request->getPost('subject');
-        $message = $this->request->getPost('message');
+        $name = $this->request->getVar('name');
+        $from = $this->request->getVar('email');
+        $subject = $this->request->getVar('subject');
+        $message = $this->request->getVar('message');
+        
+        $email = \Config\Services::email();
 
-
-        if ($this->request->getMethod() === 'post' && $this->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'subject' => 'required',
-            'message' => 'required'
-        ])) 
+        $email->setFrom($from, $name);
+        $email->setTo('abdoulazizsoumana85@gmail.com');
+        
+        
+        $email->setSubject($subject);
+        $email->setMessage($message);
+        
+        if ($email->send())
         {
-
-            $email = \Config\Services::email();
-
-            $email->setFrom($from, $name);
-            $email->setTo('abdoulazizsoumana85@gmail.com');
-
-            $email->setSubject($subject);
-            $email->setMessage($message);
-
-            if ($email->send())
-            {
-                echo 'OK';
-            }
-
+            echo 'OK';
         }
 	}
 
